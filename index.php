40a0efb8919805271597bf3e7b965ff740a513e2
danupras01 <danupras01@gmail.com>
2019-03-11 12:02:51 +0700

    perubahan gambar

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/KO.jpg');">
+		<div class="container-login100" style="background-image: url('images/O.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					PERHITUNGAN LUAS LINGKARAN
